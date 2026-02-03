c021e18ba3d96bec3f822cb2e239f06396e62156
Elda0842 <elise.a.dandrea@gmail.com>
2026-02-03 09:57:09 +0100

    camarche

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 
 export const routes: Routes = [
-  { path: 'ma-page', component: HomepageComponent }
+  { path: '', component: HomepageComponent }
 ];
